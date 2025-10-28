e0cf6d7e8d3c95ec1f3b1dd3291258feb25cb44a
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-10-28 12:58:36 -0500

    Fix formatting issues in index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Step 1: Create a variable named automobiles
-let automobiles = ['car','truck','motorcycle'];
+let automobiles = ['car', 'truck', 'motorcycle'];
 
 // Step 2: Add airplane and skateboard to the end
-automobiles.push('airplane','skateboard');
+automobiles.push('airplane', 'skateboard');
 
 // Step 3: Add bike and helicopter to the front
-automobiles.unshift('bike','helicopter');
+automobiles.unshift('bike', 'helicopter');
 
 // Step 4: Define a new variable automobilesCopy as a string template
-let automobilesCopy = `Automobiles list: ${automobiles.join(', ')}`;
+let automobilesCopy = `Automobiles list: ${automobiles.join(',')}`;
 
 // Step 5: Remove the last item
 automobiles.pop();
@@ -18,5 +18,6 @@ automobiles.shift();
 
 // Step 7: Assign the innerHTML of the element with id "result"
 document.getElementById('result').innerHTML = automobiles.join(', ');
+
 
 
