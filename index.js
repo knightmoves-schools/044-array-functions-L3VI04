f893f9a241cba1bb23001d996f97c6207d610b58
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-10-28 12:56:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Step 1: Create a variable named automobiles
-let automobiles = ['car', 'truck', 'motorcycle'];
+let automobiles = ['car','truck','motorcycle'];
 
 // Step 2: Add airplane and skateboard to the end
-automobiles.push('airplane', 'skateboard');
+automobiles.push('airplane','skateboard');
 
 // Step 3: Add bike and helicopter to the front
-automobiles.unshift('bike', 'helicopter');
+automobiles.unshift('bike','helicopter');
 
 // Step 4: Define a new variable automobilesCopy as a string template
 let automobilesCopy = `Automobiles list: ${automobiles.join(', ')}`;
@@ -18,4 +18,5 @@ automobiles.shift();
 
 // Step 7: Assign the innerHTML of the element with id "result"
 document.getElementById('result').innerHTML = automobiles.join(', ');
+
 
